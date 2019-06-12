3b2a5457b0bfb62e25c0dfd713771d27a8d104e5
Kush Shrestha <kush_shrestha01@yahoo.com>
2019-06-12 11:30:40 -0700

    lab completed

---
+++ src/main/java/com/shrestha/code401springboot/firstWebApp/HelloWorldController.java
@@ -0,0 +1,41 @@
+package com.shrestha.code401springboot.firstWebApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+// Mark this class as a controller, so that SpringBoot Knows to look at it
+@RestController
+public class HelloWorldController {
+
+    // specity the route for this method
+    @GetMapping("/")
+    public String getHelloWorld() {
+        return "Hello, World!!!";
+    }
+
+    @GetMapping("/hello/{name}/{from}")
+    public String getHelloToMe(@PathVariable String name, @PathVariable String from) {
+        return "Hello there, " + name + " from " + from;
+    }
+
+    @GetMapping("/capitalize/{string}")
+    public String capitalize(@PathVariable String string) {
+        String result = string.toUpperCase();
+        return "Capitalize: " + result;
+    }
+
+    @GetMapping("/reverse/{string}")
+    public static String reverseSentence(@PathVariable String string) {
+        String[] word = string.split(" ");;
+        String[] result = new String[word.length];
+        int j = word.length-1;
+        for (int i = 0; i < word.length; i++) {
+            result[j] = word[i];
+            j--;
+        }
+        String str = String.join(" ", result);
+        return str;
+    }
+
+}
